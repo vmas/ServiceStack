9abbeac799386523ecddcce602d2dcd65f7c3bc4
Steffen Mueller <arxisos@googlemail.com>
2012-04-20 23:27:07 +0200

    created cache for Task<> reflection logic

--- src/ServiceStack.Plugins.Tasks/TaskSupport.cs
+++ src/ServiceStack.Plugins.Tasks/TaskSupport.cs
@@ -6,6 +6,8 @@ using ServiceStack.WebHost.Endpoints;
 using ServiceStack.ServiceHost;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
 
 namespace ServiceStack.Plugins.Tasks
 {
@@ -53,42 +55,74 @@ namespace ServiceStack.Plugins.Tasks
 			}
 		}
 
+		private static ConcurrentDictionary<Tuple<Type, Type, Type>, Func<object, object, object, object>> converterCache 
+			= new ConcurrentDictionary<Tuple<Type, Type, Type>, Func<object, object, object, object>>();
+
 		public IServiceResult Convert(object service, object request, object response, Action<IServiceResult> callback)
 		{
-			var task = response as Task;
-			if (task != null)
+			var responseType = response.GetType();
+			if (responseType.GetGenericTypeDefinition() == typeof(Task<>))
 			{
-				//TODO: Cache reflection logic
-				var responseType = response.GetType();
-				var resultProperty = responseType.GetProperty("Result");
-				if (resultProperty != null)
+				var task = response as Task;
+
+				var key = new Tuple<Type, Type, Type>(service.GetType(), request.GetType(), responseType);
+				Func<object, object, object, object> getResultFn = null;
+				if (!converterCache.TryGetValue(key, out getResultFn))
 				{
-					Func<object> getResultFn = () => {
-						try
-						{
-							return resultProperty.GetValue(response, new object[] { });
-						}
-						catch (TargetInvocationException ex)
-						{
-							var aggregateException = ex.InnerException;
-							var serviceType = service.GetType();
-							var handleExceptionFn = serviceType.GetMethod("HandleException");
-							if(handleExceptionFn != null)
-								return handleExceptionFn.Invoke(service, new object[] { request, aggregateException.InnerException });
-
-							throw; //if no handle exception method exists re-throw the exception
-						}
-					};
-
-					var taskWrapper = new TaskWrapper(task, getResultFn);
-
-					//TODO: Add right synchronization context and improve exception handling
-					task.ContinueWith(t => callback(taskWrapper));
-					return taskWrapper;
+					getResultFn = this.GenerateGetResultFn(service.GetType(), request.GetType(), responseType);
+					converterCache.TryAdd(key, getResultFn);
 				}
+
+				var taskWrapper = new TaskWrapper(task, () => getResultFn(service, request, response));
+
+				task.ContinueWith(t => callback(taskWrapper));
+				return taskWrapper;
 			}
 
 			return null;
+		}
+
+		private Func<object, object, object, object> GenerateGetResultFn(Type serviceType, Type requestType, Type responseType)
+		{
+			var responseParameter = Expression.Parameter(typeof(object), "response");
+			var resultProperty = responseType.GetProperty("Result");
+
+			var getResultFn = Expression.Lambda<Func<object, object>>(
+				Expression.Property(Expression.Convert(responseParameter, responseType), resultProperty), 
+				responseParameter
+			).Compile();
+
+			var serviceParameter = Expression.Parameter(typeof(object), "service");
+			var requestParameter = Expression.Parameter(typeof(object), "request");
+			var exceptionParameter = Expression.Parameter(typeof(Exception), "ex");
+
+			var handleExceptionMethod = serviceType.GetMethod("HandleException");
+			if (handleExceptionMethod == null) //Don't add exception handling logic if HandleException() doesn't exist
+				return (service, request, response) => getResultFn(response);
+
+			var handleExceptionFn = Expression.Lambda<Func<object, object, Exception, object>>(
+				Expression.Call(
+					Expression.Convert(serviceParameter, serviceType), 
+					handleExceptionMethod, 
+					Expression.Convert(requestParameter, requestType), 
+					exceptionParameter
+				),
+				serviceParameter, requestParameter, exceptionParameter
+			).Compile();
+
+			Func<object, object, object, object> tryGetResultFn = (service, request, response) =>
+			{
+				try
+				{
+					return getResultFn(response);
+				}
+				catch (AggregateException ex)
+				{
+					return handleExceptionFn(service, request, ex.InnerException);
+				}
+			};
+
+			return tryGetResultFn;
 		}
 	}
 }
