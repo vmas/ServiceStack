6b7c06b2ce77a385dad6ac77b49249cbe137938b
Steffen Mueller <arxisos@googlemail.com>
2012-06-30 12:34:31 +0200

    added AsyncServiceBase and AsyncRestServiceBase to support async/await directly on the service ServiceBase and RestServiceBase can still be used for async services (just return Task<>)

---
+++ tests/ServiceStack.Plugins.Tasks.Tests/AsyncRestServiceBaseTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.ServiceInterface;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ServiceStack.WebHost.Endpoints;
+using ServiceStack.Service;
+using ServiceStack.ServiceClient.Web;
+
+namespace ServiceStack.Plugins.Tasks.Tests
+{
+    public class Bye { }
+
+    public class ByeResponse
+    {
+        public string Message { get; set; }
+    }
+
+    public class ByeService : AsyncRestServiceBase<Bye>
+    {
+        public override Task<object> OnPost(Bye request)
+        {
+            return Task.Factory.StartNew<object>(() =>
+            {
+                return new ByeResponse() { Message = "This was async!" };
+            });
+        }
+    }
+
+    [TestFixture]
+    public class AsyncRestServiceBaseTests
+    {
+        const string ListeningOn = "http://localhost:82/";
+
+        class AppHostHttpListener : AppHostHttpListenerBase
+        {
+            public AppHostHttpListener()
+                : base("Async service tests", typeof(AsyncService).Assembly)
+            {
+            }
+
+            public override void Configure(Funq.Container container)
+            {
+                this.LoadPlugin(new TaskSupport());
+                this.Routes.Add<Bye>("/bye");
+            }
+        }
+
+        AppHostHttpListener appHost;
+
+        [TestFixtureSetUp]
+        public void OnTestFixtureSetUp()
+        {
+            appHost = new AppHostHttpListener();
+            appHost.Init();
+            appHost.Start(ListeningOn);
+        }
+
+        [TestFixtureTearDown]
+        public void OnTestFixtureTearDown()
+        {
+            appHost.Dispose();
+        }
+
+        static IRestClient[] ServiceClients = 
+        {
+            new JsonServiceClient(ListeningOn),
+            new XmlServiceClient(ListeningOn),
+            new JsvServiceClient(ListeningOn)
+        };
+
+        [Test, TestCaseSource("ServiceClients")]
+        public void Can_retrieve_data_with_rest_endpoint(IRestClient client)
+        {
+            var response = client.Post<ByeResponse>("/bye", new Bye());
+            Assert.That(response.Message, Is.EqualTo("This was async!"));
+        }
+
+        [Test, TestCaseSource("ServiceClients")]
+        public void Can_retrieve_data_with_default_endpoint(IServiceClient client)
+        {
+            var response = client.Send<ByeResponse>(new Bye());
+            Assert.That(response.Message, Is.EqualTo("This was async!"));
+        }
+    }
+}
---
+++ tests/ServiceStack.Plugins.Tasks.Tests/AsyncServiceBaseTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using ServiceStack.ServiceInterface;
+using System.Threading.Tasks;
+using ServiceStack.Service;
+using ServiceStack.WebHost.Endpoints;
+using ServiceStack.ServiceClient.Web;
+
+namespace ServiceStack.Plugins.Tasks.Tests
+{
+    public class Hello { }
+
+    public class HelloResponse
+    {
+        public string Message { get; set; }
+    }
+
+    public class HelloService : AsyncServiceBase<Hello>
+    {
+        protected override Task<object> Run(Hello request)
+        {
+            return Task.Factory.StartNew<object>(() =>
+            {
+                return new HelloResponse() { Message = "This was async!" };
+            });
+        }
+    }
+
+    [TestFixture]
+    public class AsyncServiceBaseTests
+    {
+        const string ListeningOn = "http://localhost:82/";
+
+        class AppHostHttpListener : AppHostHttpListenerBase
+        {
+            public AppHostHttpListener()
+                : base("Async service tests", typeof(AsyncService).Assembly)
+            {
+            }
+
+            public override void Configure(Funq.Container container)
+            {
+                this.LoadPlugin(new TaskSupport());
+                this.Routes.Add<Hello>("/hello");
+            }
+        }
+
+        AppHostHttpListener appHost;
+
+        [TestFixtureSetUp]
+        public void OnTestFixtureSetUp()
+        {
+            appHost = new AppHostHttpListener();
+            appHost.Init();
+            appHost.Start(ListeningOn);
+        }
+
+        [TestFixtureTearDown]
+        public void OnTestFixtureTearDown()
+        {
+            appHost.Dispose();
+        }
+
+        static IRestClient[] ServiceClients = 
+        {
+            new JsonServiceClient(ListeningOn),
+            new XmlServiceClient(ListeningOn),
+            new JsvServiceClient(ListeningOn)
+        };
+
+        [Test, TestCaseSource("ServiceClients")]
+        public void Can_retrieve_data_with_rest_endpoint(IRestClient client)
+        {
+            var response = client.Get<HelloResponse>("/hello");
+            Assert.That(response.Message, Is.EqualTo("This was async!"));
+        }
+
+        [Test, TestCaseSource("ServiceClients")]
+        public void Can_retrieve_data_with_default_endpoint(IServiceClient client)
+        {
+            var response = client.Send<HelloResponse>(new Hello());
+            Assert.That(response.Message, Is.EqualTo("This was async!"));
+        }
+    }
+}
